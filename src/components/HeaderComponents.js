02768d00a78354450f742e78e7122d5cba2bc663
Nikita <nikita987dsa@gmail.com>
2022-07-26 22:12:14 +0530

    Header and Footer

--- src/components/HeaderComponents.js
+++ src/components/HeaderComponents.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Media,Card,CardBody,CardImg, CardTitle, CardText, CardGroup,CardImgOverlay, Row, Col } from 'reactstrap';
+
 class Header extends Component {
  render() {
  return(
@@ -10,13 +12,26 @@ class Header extends Component {
  </div>
  </Navbar>
  <div>
- <div className="container" >
+ <div className="container"  >
  <div className="row row-header">
  <div className="col-12 col-sm-6">
- <h1>Ristorante con Fusion</h1>
- <p>We take inspiration from the World's best cuisines, and create a
-unique fusion experience. Our lipsmacking creations will tickle your culinary senses!
-</p>
+    <Card className="my-2"
+    color="white"
+    outline
+    style={{
+      width: '100rem'
+    }}>
+    <CardImg alt="Ristorante con Fusion " src= "https://wallpaperaccess.com/full/271686.jpg"  style={{height:300}}/>
+      <CardImgOverlay>
+        <CardTitle style={{color:"black"}}>
+          <h5 className='d-flex p-2'>  Ristorante con Fusion </h5>
+          <p>We take inspiration from the World's best cuisines, and create a
+            unique fusion experience. Our lipsmacking creations will tickle your culinary senses!
+            </p>
+        </CardTitle>
+      </CardImgOverlay>
+    </Card>
+ 
  </div>
  </div>
  </div>
